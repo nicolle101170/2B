0848fc547058a206cb63ca589830002519e37dad
nicolle spagnol <142838881+nicolle101170@users.noreply.github.com>
2024-10-10 14:09:03 -0300

    commit

---
+++ assets/perguntas.js
@@ -0,0 +1,15 @@
+criaCartao(
+    categoria, pergunta, resposta,
+)
+
+criaCartao(
+    'programação', 'o que é javascript', 'o javaScript é uma linguagem de programacao'
+)
+
+criaCartao(
+    'Geografia', 'Qual capital da frança?', 'A capital da frança é paris'
+)
+
+criaCartao(
+    'Programação', 'O que é uma função?' 'Uma função é um bloco de código que executa alguma tarefa'
+)
